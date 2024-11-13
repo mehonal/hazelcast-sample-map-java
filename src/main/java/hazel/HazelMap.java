902dc1551f2e4c375f1448e443ac48c8d601f85b
Mehonal <116497904+mehonal@users.noreply.github.com>
2024-11-13 20:05:06 +0300

    connected to Hazelcast in HazelMap class

--- src/main/java/hazel/HazelMap.java
+++ src/main/java/hazel/HazelMap.java
@@ -6,12 +6,27 @@ import com.hazelcast.map.IMap;
 
 public class HazelMap {
     public static void main(String[] args) {
+    	// initializing Hazelcast instance
         HazelcastInstance hz = Hazelcast.newHazelcastInstance();
-        IMap<String, String> map = hz.getMap("my-distributed-map");
-        map.put("key", "value");
-        System.out.println("Value for 'key': " + map.get("key"));
-        map.putIfAbsent("key", "value");	
-        map.replace("key", "value", "newvalue");
+        
+        // getting (or making) the map
+        IMap<String, Person> map = hz.getMap("my-distributed-map");
+        
+        // looping through 10,000 randomly generated person objects
+        for (int i = 0; i < 10000; i++) {
+        	
+        	// adding to Hazelcast map
+        	int age = 1 + (int)(Math.random() * 100); // generating random age
+        	Person person = new Person("Person " + i, age);
+        	map.put("person-" + i, person); // adding to Hazelcast Map
+        	System.out.println(i + ". Added: " + person);
+        	
+        }
+        for (int i = 0; i < 10; i++) {
+        	// looping through first 10 persons from the map
+        	System.out.println(map.get("person-" + i));
+        }
+        System.out.println("Value for 'person-20': " + map.get("person-20"));
         hz.shutdown();
     }
 }
